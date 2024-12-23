dc55320cd8c399af3cf4d5334b5308b7f1a80167
ungs12 <ungilseon@gmail.com>
2024-12-23 22:13:54 +0900

    Title: 따옴표 출력, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

--- 241223/따옴표 출력/print-quote.java
+++ 241223/따옴표 출력/print-quote.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        System.out.println("He says \"it's a really simple sentence\".");
+        System.out.println("He says \"It's a really simple sentence\".");
     }
 }
