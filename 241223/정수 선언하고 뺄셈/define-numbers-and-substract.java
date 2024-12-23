2ae5a72ece889c98849e1aa972b99f3550dea230
ungs12 <ungilseon@gmail.com>
2024-12-23 22:22:17 +0900

    Title: 정수 선언하고 뺄셈, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241223/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 241223/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -3,6 +3,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a = 97;
         int b = 13;
-        System.out.println(a - b);
+        System.out.println("a - b = " + a - b);
     }
 }
