81472ec07ffe6aa2e9704e430a3bf024a469ce3d
ungs12 <ungilseon@gmail.com>
2024-12-23 23:10:07 +0900

    Title: 변수 값 복사하기, Time: 90ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241223/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1, b = 2, c = 3;
+        b = a = c;
+        System.out.print(a + " " + b + " " + c);
+    }
+}
