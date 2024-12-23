cfc9b4032ef85e781b937bbd159deacf68d452c7
ungs12 <ungilseon@gmail.com>
2024-12-23 22:26:41 +0900

    Title: 변수 선언하기, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

--- 241223/변수 선언하기/declaring-variables.java
+++ 241223/변수 선언하기/declaring-variables.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         int a = 3;
-        String b = 'C';
+        char b = 'C';
 
         System.out.println(a);
         System.out.println(b);
