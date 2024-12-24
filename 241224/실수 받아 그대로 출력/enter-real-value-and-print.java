09debf23a6e1684901da0b6f432dd7aa09070b40
ungs12 <ungilseon@gmail.com>
2024-12-24 23:35:49 +0900

    Title: 실수 받아 그대로 출력, Time: 155ms, Memory: 11MB, Status: Passed - CodeTree

--- 241224/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 241224/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double n = sc.nextDouble();
-        System.out.printf(%.2f, n);
+        System.out.printf("%.2f", n);
     }
 }
