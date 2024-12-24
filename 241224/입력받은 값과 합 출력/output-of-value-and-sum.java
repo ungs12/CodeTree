65374a0aab8e65f3778bc9f6bbb5f268f3794424
ungs12 <ungilseon@gmail.com>
2024-12-24 23:49:34 +0900

    Title: 입력받은 값과 합 출력, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241224/입력받은 값과 합 출력/output-of-value-and-sum.java
+++ 241224/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -5,6 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.print(a + " " + b + " " + a + b);
+        System.out.print(a + " " + b + " " + (a + b));
     }
 }
