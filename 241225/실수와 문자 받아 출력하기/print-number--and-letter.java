8816ba21e0118975f07803b7970199772d09abd0
ungs12 <ungilseon@gmail.com>
2024-12-25 13:35:23 +0900

    Title: 실수와 문자 받아 출력하기, Time: 158ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241225/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(s);
+        System.out.printf("%.2f", a);
+        System.out.println();
+        System.out.printf("%.2f", b);
+    }
+}
