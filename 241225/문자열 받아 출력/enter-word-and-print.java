cb53cd0a5c8265eb258c7b910f82ccfaff4a4dcd
ungs12 <ungilseon@gmail.com>
2024-12-25 13:29:53 +0900

    Title: 문자열 받아 출력, Time: 115ms, Memory: 10MB, Status: Passed - CodeTree

--- 241225/문자열 받아 출력/enter-word-and-print.java
+++ 241225/문자열 받아 출력/enter-word-and-print.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = s.next();
+        String s = sc.next();
         System.out.print(s);
     }
 }
