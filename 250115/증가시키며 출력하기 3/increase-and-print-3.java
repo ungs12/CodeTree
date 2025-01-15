f5c1ba2ca87baffe580388e093c79f59824f47f1
ungs12 <ungilseon@gmail.com>
2025-01-15 10:08:20 +0900

    Title: 증가시키며 출력하기 3, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250115/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        while(n <= 26){
+            System.out.print(n + " ");
+            n += 2;
+        }
+    }
+}
