e7587995ddc8e1cfb57b0e7cb2da9bd627d840f4
ungs12 <ungilseon@gmail.com>
2025-01-15 19:50:23 +0900

    Title: 감소시키며 출력하기 2, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250115/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 26;
+
+        while(a >= 10){
+            System.out.print(a + " ");
+            a--;
+        }
+    }
+}
